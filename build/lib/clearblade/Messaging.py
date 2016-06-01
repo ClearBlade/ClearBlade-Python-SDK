4369a818336bf4bfb2d6f811aaf15f41f2cc5a6c
Ameya Chikodi <achikodi@clearblade.com>
2016-06-01 17:50:56 -0500

    Updates on error code 4

--- build/lib/clearblade/Messaging.py
+++ build/lib/clearblade/Messaging.py
@@ -12,11 +12,14 @@ from urlparse import urlparse
 class Messaging():
 	CB_MSG_ADDR = ""
 	response = 0
+	keep_alive = 30
+	subscribeDict = dict()
 	def __init__(self, clientType):
 		self.client = ""
 		self.rc = 0
 		self.clientType = clientType
 		self.CB_MSG_ADDR = urlparse(clientType.platform).netloc
+		self.auth = auth.Auth()
 
 	def printValue(self):
 		if isinstance(self.clientType, Client.UserClient):
@@ -25,8 +28,9 @@ class Messaging():
 			print self.clientType.email	
 
 	def InitializeMQTT(self, **keyword_parameters):
+		print("Inside initialize")
 		if isinstance(self.clientType, Client.UserClient):
-			self.client = mqtt.Client(client_id=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(23)), protocol=mqtt.MQTTv311)
+			self.client = mqtt.Client(client_id=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(23)), clean_session=True, userdata=None, protocol=mqtt.MQTTv311)
 			self.client.username_pw_set(self.clientType.UserToken, self.clientType.systemKey)
 			print self.clientType.UserToken
 		if isinstance(self.clientType, Client.DevClient):
@@ -41,18 +45,21 @@ class Messaging():
 				print userdata
 				print client
 				print "Connected successfully "
+
+				for topic, qos in self.subscribeDict.iteritems():
+					self.subscribeNew(topic,qos)
+					print "SUBSCRIBING "+topic+", "+str(qos)
 			else:
-				print "Here.. Error in connection with code ", rc
-				global auth = auth.Auth()
-				auth.Authenticate(Client.UserClient)
-				print "User Authenticated"
+				print "Error in connection with code ", str(rc)+"... Trying to reconnect"
+				self.reconnectFunction()
+			
 
 		def on_log(client, userdata, level, buf):
 			print "Inside log : ", buf	
 
 		self.client.on_connect = on_connect
 		self.on_log = on_log
-		
+
 		if ('keep_alive' in keyword_parameters):
 			print "Timeout is : ", keyword_parameters['keep_alive']
 			self.client.connect(self.CB_MSG_ADDR, 1883, keyword_parameters['keep_alive'])
@@ -79,7 +86,15 @@ class Messaging():
 
 	def subscribe(self, topic, qos):
 		if isinstance(self.clientType, Client.UserClient) or isinstance(self.clientType, Client.DevClient):
+			if topic in self.subscribeDict:
+				print "Already subscribed to the topic"
+			else:
+				self.subscribeDict[topic] = qos
 			thread.start_new_thread(self.keepSubscribed, (topic,qos))
+
+	def subscribeNew(self, topic, qos):
+		if isinstance(self.clientType, Client.UserClient) or isinstance(self.clientType, Client.DevClient):
+			thread.start_new_thread(self.keepSubscribed, (topic,qos))		
 		
 	def keepSubscribed(self,topic,qos):
 
@@ -97,6 +112,7 @@ class Messaging():
 		if isinstance(self.clientType, Client.UserClient) or isinstance(self.clientType, Client.DevClient):
 			print("Inside unsubscribed")
 			self.client.unsubscribe(topic)
+			self.subscribeDict.pop(topic, None)
 			def on_unsubscribe(client, userdata, mid):
 				print "Unsubscribed ",mid
 			self.client.on_unsubscribe = on_unsubscribe
@@ -108,3 +124,24 @@ class Messaging():
 			self.client.disconnect()
 			self.client.on_disconnect = on_disconnect
 			self.client.loop_stop()
+
+	def reconnectFunction(self):
+		print("Inside reconnect function")
+		#userClient = Client.UserClient("cecdeef40a98c1e1cb87c58dad58", "CECDEEF40A869A818AC6D9D4C21F", "parent@acme.com", "edge", "http://localhost") 
+		userClient = self.clientType
+		self.auth.Authenticate(userClient)
+		self.clientType = userClient
+		if isinstance(self.clientType, Client.UserClient):
+			#self.client = mqtt.Client(client_id=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(23)), clean_session=True, userdata=None, protocol=mqtt.MQTTv311)
+			#self.client.reinitialise(client_id=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(23)), clean_session=True, userdata=None)
+			self.client.disconnect()
+			self.client.username_pw_set(self.clientType.UserToken, self.clientType.systemKey)
+			#print "###"+self.clientType.UserToken+"###"
+			#print self.clientType.systemKey
+			#self.client.connect(self.CB_MSG_ADDR, 1883, self.keep_alive)
+			self.client.reconnect()
+			self.client.loop_start()
+			print "reinitialise"
+		if isinstance(self.clientType, Client.DevClient):
+			self.client = mqtt.Client(client_id=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(23)), protocol=mqtt.MQTTv311)
+			self.client.username_pw_set(self.clientType.DevToken, self.clientType.systemKey)
