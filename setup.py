b021b1fe6394bbff0b9de66ffacfe69bed54c1d1
Michael Sprague <msprague@clearblade.com>
2018-01-12 10:27:28 -0700

    up version to 2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='clearblade',
     packages=['clearblade'],
     install_requires=['requests', 'paho-mqtt>=1.3.0'],
-    version='2.0',
+    version='2.1',
     description='A Python SDK for interacting with the ClearBlade Platform.',
     url='https://github.com/ClearBlade/ClearBlade-Python-SDK',
     download_url='https://github.com/ClearBlade/ClearBlade-Python-SDK/archive/v2.0.tar.gz',
