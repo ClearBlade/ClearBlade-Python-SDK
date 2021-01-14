769f8df0ee678568c69e4d9f9942ddb2823e2fc4
skysharma <sky.sharma@gmail.com>
2021-01-14 13:44:55 -0600

    Version 2.3.0 -> 2.4.0

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='clearblade',
     packages=['clearblade'],
     install_requires=['requests', 'paho-mqtt>=1.3.0'],
-    version='2.3.0',
+    version='2.4.0',
     description='A Python SDK for interacting with the ClearBlade Platform.',
     url='https://github.com/ClearBlade/ClearBlade-Python-SDK',
-    download_url='https://github.com/ClearBlade/ClearBlade-Python-SDK/archive/v2.3.0.tar.gz',
+    download_url='https://github.com/ClearBlade/ClearBlade-Python-SDK/archive/v2.4.0.tar.gz',
     keywords=['clearblade', 'iot', 'sdk'],
     maintainer='Aaron Allsbrook',
     maintainer_email='dev@clearblade.com'
