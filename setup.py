80a71c9d3351a83d616ae85c89fc77d7d19f2ff3
Jim Bouquet <jbouquet@clearblade.com>
2024-03-07 08:25:57 -0600

    Added a variable for version

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+version = '2.4.5'
 
 setup(
     name='clearblade',
     packages=['clearblade'],
     install_requires=['requests', 'paho-mqtt>=1.3.0'],
-    version='2.4.5',
+    version=[version],
     description='A Python SDK for interacting with the ClearBlade Platform.',
     url='https://github.com/ClearBlade/ClearBlade-Python-SDK',
-    download_url='https://github.com/ClearBlade/ClearBlade-Python-SDK/archive/v2.4.4.tar.gz',
+    download_url='https://github.com/ClearBlade/ClearBlade-Python-SDK/archive/' + version + '.tar.gz',
     keywords=['clearblade', 'iot', 'sdk'],
     maintainer='Aaron Allsbrook',
     maintainer_email='dev@clearblade.com'
