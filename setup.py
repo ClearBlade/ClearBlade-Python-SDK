adf5934950c39201ac564f409561e7141aa5395f
Jim Bouquet <jbouquet@clearblade.com>
2024-03-07 15:37:05 -0600

    Removed brackets wrongly added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='clearblade',
     packages=['clearblade'],
     install_requires=['requests', 'paho-mqtt>=1.3.0'],
-    version=[version],
+    version=version,
     description='A Python SDK for interacting with the ClearBlade Platform.',
     url='https://github.com/ClearBlade/ClearBlade-Python-SDK',
     download_url='https://github.com/ClearBlade/ClearBlade-Python-SDK/archive/v' + version + '.tar.gz',
